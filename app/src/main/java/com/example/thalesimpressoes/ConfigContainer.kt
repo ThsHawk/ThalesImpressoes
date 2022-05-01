d756480c7b723414684d4d137a0a3293a6de8a7b
ThsHawk <thalesadsilva@outlook.com>
2022-05-01 00:52:43 -0300

    Layout 1.1

---
+++ app/src/main/java/com/example/thalesimpressoes/ConfigContainer.kt
@@ -0,0 +1,11 @@
+package com.example.thalesimpressoes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigContainer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config_container)
+    }
+}
