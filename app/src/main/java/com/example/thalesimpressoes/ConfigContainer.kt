0d0c22e78e99b086c4ebafe2a421441149fdced4
ThsHawk <thalesadsilva@outlook.com>
2022-05-02 09:25:34 -0300

    Logic 1.00

--- app/src/main/java/com/example/thalesimpressoes/ConfigContainer.kt
+++ app/src/main/java/com/example/thalesimpressoes/ConfigContainer.kt
@@ -1,11 +1,40 @@
 package com.example.thalesimpressoes
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ConfigContainer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config_container)
-    }
-}
+
+        val sharedPref = getSharedPreferences("appscriptID", Context.MODE_PRIVATE)
+        val appscriptID = sharedPref.getString("ID", null)
+        val id = appscriptID.toString()
+        val editor = sharedPref.edit()
+
+        val appscriptIDfield = findViewById<EditText>(R.id.appscripts_ID)
+        if(appscriptID != null) appscriptIDfield.hint = id
+
+        val confirmBtn = findViewById<Button>(R.id.appscripts_confirmButton)
+
+
+        confirmBtn.setOnClickListener{
+
+            val appscriptID = appscriptIDfield.text.toString()
+            editor.apply{
+                putString("ID", appscriptID)
+                apply()
+            }//editor
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+
+        }//confirmBtn
+
+    }//onCreate
+}//ConfigContainer
--- app/src/main/java/com/example/thalesimpressoes/MainActivity.kt
+++ app/src/main/java/com/example/thalesimpressoes/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.thalesimpressoes
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        val sendButton = findViewById<Button>(R.id.send_button)
+        val configBtn = findViewById<ImageButton>(R.id.config_button)
+        val okButton = findViewById<TextView>(R.id.okButton)
+
+        sendButton.setOnClickListener{
+            sendEntry()
+        }//sendButton
+        configBtn.setOnClickListener{
+            val intent = Intent(applicationContext, ConfigContainer::class.java)
+            startActivity(intent)
+        }//configBtn
+        okButton.setOnClickListener {
+            val entry = findViewById<EditText>(R.id.data_entry)
+            entry.text = null
+            okButton.visibility = View.GONE
+        }//okButton
+    }//onCreate
+
+    private fun sendEntry(){
+        val entry = findViewById<EditText>(R.id.data_entry)
+        val value = entry.text.toString()
+        if(value.isEmpty()) return
+
+        val sharedPref = getSharedPreferences("appscriptID", Context.MODE_PRIVATE)
+        val appscriptID = sharedPref.getString("ID", null)
+
+        Thread{
+            val url = URL("https://script.google.com/macros/s/${appscriptID}/exec?entry=${"%.2".format(value.toDouble())}")
+            val conn = url.openConnection() as HttpsURLConnection
+
+            try {
+                /*val data = conn.inputStream.bufferedReader().readText()
+                if(data == "Entry Written on column C"){
+                    val okButton = findViewById<TextView>(R.id.okButton)
+                    okButton.visibility = View.VISIBLE
+                }//if*/
+
+            }finally {
+                conn.disconnect()
+            }//try
+
+        }.start()//Thread
+    }//sendEntry
+
+
+}//MainActivity
